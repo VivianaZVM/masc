8c82760715bc20aac08e57885d6dc7050ac9f646
lxlChris <117472929+lxlChris@users.noreply.github.com>
2022-11-11 11:22:41 -0600

    Add files via upload

--- solicitud_registro.sql
+++ solicitud_registro.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.0
+-- version 5.1.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 09-11-2022 a las 15:22:35
--- Versión del servidor: 10.4.24-MariaDB
--- Versión de PHP: 8.1.6
+-- Tiempo de generación: 11-11-2022 a las 03:45:21
+-- Versión del servidor: 10.4.19-MariaDB
+-- Versión de PHP: 8.0.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -29,10 +29,20 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `Nombre` varchar(30) NOT NULL,
-  `Correo` varchar(50) NOT NULL,
-  `Telefono` int(10) NOT NULL
+  `Nombre` varchar(500) NOT NULL,
+  `Correo` varchar(500) NOT NULL,
+  `Telefono` varchar(200) NOT NULL,
+  `Direccion` varchar(500) NOT NULL,
+  `Ocacion` varchar(500) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id`, `Nombre`, `Correo`, `Telefono`, `Direccion`, `Ocacion`) VALUES
+(8, 'Luis Antonio ', 'aaa@gmail.com', '4494244243', 'Palmas', 'XV'),
+(10, 'Chris', 'b@gmail.com', '4494263984', 'Lomas', 'Infantil');
 
 --
 -- Índices para tablas volcadas
@@ -52,7 +62,7 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
