04cf3175b0d481b7863077599bfe3453574b6bfa
MAZC90 <20301061550115@cetis155.edu.mx>
2022-11-18 19:05:20 -0600

    listooo

--- inicio.py
+++ inicio.py
@@ -23,6 +23,32 @@ def Contactos():
 def contacto():
     return render_template("contacto.html")
 
+@app.route('/insertar')
+def insertar():
+    return render_template("insertar.html")
+
+@app.route('/crud')
+def crud():
+    conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro')
+    cursor = conn.cursor()
+    cursor.execute('select id, Nombre, Correo, Telefono, Direccion, Ocacion from usuarios order by id')
+    datos = cursor.fetchall()
+    return render_template("crud.html", comentar = datos)
+
+@app.route('/agrega_comenta', methods=['POST'])
+def agrega_comenta():
+    if request.method == 'POST':
+        aux_Nombre = request.form['Nombre']
+        aux_Correo = request.form['Correo']
+        aux_Telefono = request.form['Telefono']
+        aux_Direccion = request.form['Direccion']
+        aux_Ocacion = request.form['Ocacion']
+        conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro')
+        cursor = conn.cursor()
+        cursor.execute('insert into usuarios (Nombre,Correo,Telefono,Direccion,Ocacion) values (%s, %s, %s, %s, %s)',(aux_Nombre, aux_Correo, aux_Telefono, aux_Direccion, aux_Ocacion))
+        conn.commit()
+    return redirect(url_for('index'))
+
 @app.route('/editar/<string:id>')
 def editar(id):
     conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro')
@@ -53,31 +79,31 @@ def borrar(id):
     conn.commit()
     return redirect(url_for('crud'))
 
-@app.route('/insertar')
-def insertar():
-    return render_template("insertar.html")
-
-@app.route('/crud')
-def crud():
-    conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro')
-    cursor = conn.cursor()
-    cursor.execute('select id, Nombre, Correo, Telefono, Direccion, Ocacion from usuarios order by id')
-    datos = cursor.fetchall()
-    return render_template("crud.html", comentar = datos)
-
-@app.route('/agrega_comenta', methods=['POST'])
-def agrega_comenta():
-    if request.method == 'POST':
-        aux_Nombre = request.form['Nombre']
-        aux_Correo = request.form['Correo']
-        aux_Telefono = request.form['Telefono']
-        aux_Direccion = request.form['Direccion']
-        aux_Ocacion = request.form['Ocacion']
-        conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro')
-        cursor = conn.cursor()
-        cursor.execute('insert into usuarios (Nombre,Correo,Telefono,Direccion,Ocacion) values (%s, %s, %s, %s, %s)',(aux_Nombre, aux_Correo, aux_Telefono, aux_Direccion, aux_Ocacion))
-        conn.commit()
-    return redirect(url_for('index'))
+@app.route('/puesto_fdetalle/<string:id>', methods=['GET']) 
+def puesto_fdetalle(id): 
+   conn = pymysql.connect(host='localhost', user='root', passwd='', db='solicitud_registro') 
+   cursor = conn.cursor() 
+   
+   cursor.execute('select id from usuarios order by id') 
+   datos = cursor.fetchall() 
+   
+   cursor.execute('select id,Nombre,Correo,Telefono,Direccion,Ocacion from usuarios where id = %s', (id)) 
+   dato = cursor.fetchall()
+   cursor.execute('select Nombre from usuarios') 
+   datos1 = cursor.fetchall() 
+   
+   cursor.execute('select Correo from usuarios') 
+   datos2 = cursor.fetchall() 
+   
+   cursor.execute('select Telefono from usuarios') 
+   datos3 = cursor.fetchall() 
+   
+   cursor.execute('select Direccion from usuarios') 
+   datos4 = cursor.fetchall() 
+   
+   cursor.execute('select Ocacion from usuarios') 
+   datos5 = cursor.fetchall()  
+   return render_template("crud.html", pue = datos, dat=dato[0], Nombre=datos1[0], Correo=datos2[0], Telefono=datos3[0], Direccion=datos4[0], Ocacion=datos5[0]) 
 
 @app.route('/carrito')
 def carrito():
